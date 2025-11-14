7ef243d817aacea6a1e703f1ca9faef5483d5d25
giovanisims <giovani@simoes.pro>
2025-11-14 18:10:40 -0300

    added a class to run both benchmarks

---
+++ src/SyncVsNoSyncRaceCondition.java
@@ -0,0 +1,9 @@
+public class SyncVsNoSyncRaceCondition {
+    public static void main(String[] args) throws Exception {
+        System.out.println("=== Race Condition (No Sync) ===");
+        CorridaSemControle.main(args);
+
+        System.out.println("\n=== With Semaphore (Synchronized) ===");
+        CorridaComSemaphore.main(args);
+    }
+}
